1ba6e9a6422c210ea788eb29ebba2fd552b29248
Max Buggins <max.buggins@gmail.com>
2022-07-05 11:36:02 +0930

    Forced mute sound if volume slider is at end and volume dB displays to the settings UI

--- Assets/Code/UI/UI_Volume.cs
+++ Assets/Code/UI/UI_Volume.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Audio;
+using TMPro;
 
 public class UI_Volume : UI_Base
 {
+    public TextMeshProUGUI volumeText;
+
     public AudioMixerGroup audioGroup;
     private Slider slider;
 
@@ -15,11 +18,22 @@ public class UI_Volume : UI_Base
         float value;
         audioGroup.audioMixer.GetFloat(audioGroup.name, out value);
         slider.value = value;
+
+        volumeText.text = value.ToString("F1") + " dB";
     }
 
     public void UpdateValue(float value)
     {
-        audioGroup.audioMixer.SetFloat(audioGroup.name, value);
+        if (value <= -50)
+        {
+            audioGroup.audioMixer.SetFloat(audioGroup.name, -100);
+            volumeText.text = "Muted";
+        }
+        else
+        {
+            audioGroup.audioMixer.SetFloat(audioGroup.name, value);
+            volumeText.text = value.ToString("F1") + " dB";
+        }
     }
 }
 
