4d3dcf47d2a989aa621f6700802dbf67291a7fb1
Max Buggins <max.buggins@gmail.com>
2022-07-08 12:08:11 +0930

    Reworked corpse disabling

--- Assets/Code/PlayerCorpse.cs
+++ Assets/Code/PlayerCorpse.cs
@@ -6,18 +6,26 @@ using UnityEngine;
 
 public class PlayerCorpse : MonoBehaviour
 {
+    [SerializeField] private float tryDisableDelay = 4;
+    [SerializeField] private float maxDisableVelocity = 1;
+
     public Sprite[] deathSprites;
     public float[] frameDelays;
+
 
     private int frameIndex = 0;
 
     private DirectionalSprite directionalSprite;
+    private Rigidbody rb;
 
     void Start()
     {
         directionalSprite = GetComponent<DirectionalSprite>();
+        rb = GetComponentInParent<Rigidbody>();
 
         Invoke(nameof(NextFrame), frameDelays[0]);
+        Invoke(nameof(TryDisable), tryDisableDelay);
+
     }
 
     void NextFrame()
@@ -30,5 +38,17 @@ public class PlayerCorpse : MonoBehaviour
 
         Invoke(nameof(NextFrame), frameDelays[frameIndex]);
 
+    }
+
+    void TryDisable()
+    {
+        if (rb.velocity.magnitude > maxDisableVelocity)
+        {
+            Invoke(nameof(TryDisable), tryDisableDelay);
+            return;
+        }
+
+        transform.parent = null;
+        Destroy(rb.gameObject);   
     }
 }
